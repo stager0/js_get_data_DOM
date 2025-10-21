92bb5db2dd2cce8969b8991ef1609c4f5eafd77c
Andrii <andriishtaher@gmail.com>
2025-10-21 20:18:26 +0200

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,63 @@
 'use strict';
 
 // write your code here
-const countriesOl = document.querySelectorAll('.list__item');
+const countriesPopulations = document.querySelectorAll('.population');
 
 let totalPopulation = 0;
 let averagePopulation = 0;
 const populationList = [];
+const separator = countriesPopulations[0].textContent.match(/\D/)[0];
 
-for (const countryHtml of countriesOl) {
-  const population = Number(
-    countryHtml.querySelector('.population').textContent.replace(/,/g, ''),
-  );
+for (const countryHtml of countriesPopulations) {
+  const population = Number(countryHtml.textContent.replace(/\D/g, ''));
 
-  populationList.push(Number(population));
+  if (isFinite(population)) {
+    populationList.push(population);
 
-  totalPopulation = totalPopulation + Number(population);
+    totalPopulation = totalPopulation + population;
+  }
 }
 
-averagePopulation = totalPopulation / populationList.length;
+averagePopulation = Math.round(totalPopulation / countriesPopulations.length);
+
+const listAveragePopulation = String(averagePopulation).split('').reverse();
+const listTotalPopulation = String(totalPopulation).split('').reverse();
+const newListTotalPopulation = [];
+const newListAveragePopulation = [];
+
+for (let i = 0; i <= listTotalPopulation.length; i++) {
+  const num1 = listTotalPopulation[i];
+  const num2 = listAveragePopulation[i];
+
+  if (num1 || num2) {
+    if ((i + 1) % 3 === 0) {
+      if (listTotalPopulation[i + 1]) {
+        newListTotalPopulation.push(separator + num1);
+      } else {
+        newListTotalPopulation.push(num1);
+      }
+
+      if (num2) {
+        if (listAveragePopulation[i + 1]) {
+          newListAveragePopulation.push(separator + num2);
+        } else {
+          newListAveragePopulation.push(num2);
+        }
+      }
+    } else {
+      newListTotalPopulation.push(num1);
+
+      if (num2) {
+        newListAveragePopulation.push(num2);
+      }
+    }
+  }
+}
 
 const totalHtmlObj = document.querySelector('.total-population');
 const averageHtmlObj = document.querySelector('.average-population');
 
-averageHtmlObj.textContent = averagePopulation.toLocaleString();
-totalHtmlObj.textContent = totalPopulation.toLocaleString();
+averageHtmlObj.textContent = String(
+  newListAveragePopulation.reverse().join(''),
+);
+totalHtmlObj.textContent = String(newListTotalPopulation.reverse().join(''));
