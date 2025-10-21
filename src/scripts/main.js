cea364490fcec01189367ce271a8230cb99f62c1
Andrii <andriishtaher@gmail.com>
2025-10-21 18:50:38 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write your code here
+const countriesOl = document.querySelectorAll('.list__item');
+
+let totalPopulation = 0;
+let averagePopulation = 0;
+const populationList = [];
+
+for (const countryHtml of countriesOl) {
+  const population = Number(
+    countryHtml.querySelector('.population').textContent.replace(/,/g, ''),
+  );
+
+  populationList.push(Number(population));
+
+  totalPopulation = totalPopulation + Number(population);
+}
+
+averagePopulation = totalPopulation / populationList.length;
+
+const totalHtmlObj = document.querySelector('.total-population');
+const averageHtmlObj = document.querySelector('.average-population');
+
+averageHtmlObj.textContent = averagePopulation.toLocaleString();
+totalHtmlObj.textContent = totalPopulation.toLocaleString();
